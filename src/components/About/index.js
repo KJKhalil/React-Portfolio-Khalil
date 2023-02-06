19f8460a7428c77c3968c3a73bceeca79ac4e8ae
TacTac <110001003+KJKhalil@users.noreply.github.com>
2023-02-06 00:03:11 -0500

    Finished the index.js in About

---
+++ src/components/About/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function About() {
+    return(
+        <section className='row justify-content-center' id='aboutMe-container'>
+            
+            <h1 id='aboutMe'>About Me</h1>
+
+            <p className='text-background' id='aboutMeText'>
+                Hey there! My name is Khalil Khalil. I'm an aspiring developer wether that means
+                full stack, front end, or back end. I want to grow, learn, and improve as much
+                as I can in development and I'm looking for a company that encourages and nutures that growth.
+                <br></br>
+                <br></br>
+                I am currently attending the Case Western Reserve University coding boot camp learning
+                as much as I can. All the work and projects I've done so far are available for you to
+                for you to check out. I look forward to meeting and working with you soon!
+            </p>
+        </section>
+    )
+}
+
+export default About;
