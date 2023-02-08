4614bf44b1161f20f52082122cda75a367d787a9
TacTac <110001003+KJKhalil@users.noreply.github.com>
2023-02-07 23:05:40 -0500

    finished most of index.js in footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Footer () {
+    return (
+        <footer className='footer'>
+            <nav>
+                <ul className='navigation'>
+                    <li className='navLinks'>
+                        <a href='https://github.com/KJKhalil'>Github</a>
+                    </li>
+                    <li className='navLinks'>
+                        <a href='mailto: khalil.j.khalil2@gmail.com'>Email</a>
+                    </li>
+                </ul>
+            </nav>
+        </footer>
+    )
+}
+
+export default Footer
